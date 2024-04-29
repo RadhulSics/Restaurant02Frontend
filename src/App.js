a9a6b56f9e6564fdc7bf98b4cf5b08c157fa4518
sreedevi-12 <lechusreedevi@gmail.com>
2024-04-29 10:13:04 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,53 +1,47 @@
-<<<<<<< HEAD
-// import logo from './logo.svg';
-// import './App.css';
-import React from "react";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "bootstrap/dist/css/bootstrap.min.css";
-// import Landingpage1 from"./components/Landingpage1"
-// import "./components/Landingpage1.css"
-// import Landingpage from "./components/Landingpage"
-// import "./components/Landingpage.css"
-import  CustomerRegistration from"./components/CustomerRegistration"
-import "./components/CustomerRegistration.css"
-// import HomePage from "./components/Homepage";
-// import "./components/Homepage.css"
-=======
 import React from "react";
 import {BrowserRouter,Routes, Route } from "react-router-dom";
 
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Landingpage from "./components/Landingpage"
+
+import Landingpage1 from "./components/Landingpage1"
+
+import  CustomerRegistration from"./components/CustomerRegistration";
+
 import CustomerForgotPass from "./components/CustomerForgotpass";
 
 import Customerlogin from "./components/CustomerLogin";
 
-import Adminlogin from "./components/Adminlogin"
+import Adminlogin from "./components/Adminlogin";
+
+import StaffviewBooking from "./components/StaffviewBooking";
 
 
 
->>>>>>> 424746d3c52afca12b712d0cfc7da9bcbbbfa9ca
+
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      {/* <Landingpage1/>
-      // {/* // <Landingpage/> */} 
-      <CustomerRegistration/>    
-      {/* <HomePage/> */}
-=======
+
      <BrowserRouter>
-     
+    <Landingpage/>
       <Routes>
+       
+        <Route path="/" element={<Landingpage1/>}/>
+        <Route path="/CustomerRegistration" element={<CustomerRegistration/>}/>
         <Route path="/CustomerForgotpass" element={<CustomerForgotPass/>}/>
         <Route path="/Customerlogin" element={<Customerlogin/>}/>
         <Route path="/Adminlogin" element={<Adminlogin/>}/>
+        <Route path="/StaffviewBooking" element={<StaffviewBooking/>}/>
+        
+
      </Routes>
      </BrowserRouter>
->>>>>>> 424746d3c52afca12b712d0cfc7da9bcbbbfa9ca
+
 
     </div>
   );
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import"./Homepage.css"
 function Nav() {
   return (
     <div>
